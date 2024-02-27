e6cea21a2b0d4f0b6d341489773487253eeed05d
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-02-26 23:57:59 -0800

    Add Image model, NetworkManager and ImageCacheManager

---
+++ Pixify/Managers/ApiManager.swift
@@ -0,0 +1,13 @@
+//
+//  ApiManager.swift
+//  Pixify
+//
+//  Created by Ani's Mac on 26.02.24.
+//
+
+import Foundation
+
+struct ApiManager {
+    static let baseUrl = "https://pixabay.com/api/?key="
+    static let apiKey = "42585935-49ef4973398606c4a226ccd58"
+}
