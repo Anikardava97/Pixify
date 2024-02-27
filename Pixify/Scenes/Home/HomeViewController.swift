c33a38088289d0dac231fa217d56925a9f5189f2
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-02-27 02:10:58 -0800

    Fetch data from Api and configure collection view cell

--- Pixify/Scenes/Home/HomeViewController.swift
+++ Pixify/Scenes/Home/HomeViewController.swift
@@ -9,12 +9,46 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     // MARK: - Methods
+    private let viewModel = HomeViewModel()
+    private var images = [Image]()
+    
+    private let mainStackView: UIStackView = {
+        let stackView = UIStackView()
+        stackView.axis = .vertical
+        stackView.spacing = 16
+        stackView.translatesAutoresizingMaskIntoConstraints = false
+        return stackView
+    }()
+    
+    private let headerLabel: UILabel = {
+        let label = UILabel()
+        label.text = "Discover Inspiring Photos"
+        label.textColor = .customTextColor
+        label.textAlignment = .left
+        label.font = UIFont.systemFont(ofSize: 24, weight: .bold)
+        return label
+    }()
+    
+    private let collectionView: UICollectionView = {
+        let layout = UICollectionViewFlowLayout()
+        layout.scrollDirection = .vertical
+        layout.minimumInteritemSpacing = 16
+        layout.minimumLineSpacing = 16
+        layout.sectionInset = UIEdgeInsets(top: 16, left: 0, bottom: 16, right: 0)
+        
+        let collectionView = UICollectionView(frame: .zero, collectionViewLayout: layout)
+        collectionView.backgroundColor = .clear
+        collectionView.showsVerticalScrollIndicator = false
+        collectionView.translatesAutoresizingMaskIntoConstraints = false
+        return collectionView
+    }()
     
     // MARK: - ViewLifecycle
     override func viewDidLoad() {
         super.viewDidLoad()
         hideBackButton()
         setup()
+        viewModel.viewDidLoad()
     }
     
     // MARK: - Private Methods
@@ -23,22 +57,105 @@ final class HomeViewController: UIViewController {
     }
     
     private func setup() {
+        setupViewModelDelegate()
         setupBackground()
+        setupCollectionView()
         setupSubviews()
         setupConstraints()
+    }
+    
+    private func setupViewModelDelegate() {
+        viewModel.delegate = self
     }
     
     private func setupBackground() {
         view.backgroundColor = .customBackgroundColor
     }
     
+    private func setupCollectionView() {
+        collectionView.dataSource = self
+        collectionView.delegate = self
+        collectionView.register(ImagesCollectionViewCell.self, forCellWithReuseIdentifier: "imageCell")
+    }
+    
     private func setupSubviews() {
-        
+        view.addSubview(mainStackView)
+        mainStackView.addArrangedSubview(headerLabel)
+        mainStackView.addArrangedSubview(collectionView)
     }
     
     private func setupConstraints() {
         NSLayoutConstraint.activate([
-           
+            mainStackView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 32),
+            mainStackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 16),
+            mainStackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -16),
+            mainStackView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
         ])
     }
+}
+
+// MARK: - Extension: UICollectionViewDataSource
+extension HomeViewController: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        images.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "imageCell", for: indexPath) as? ImagesCollectionViewCell else {
+            return UICollectionViewCell()
+        }
+        
+        let image = images[indexPath.row]
+        cell.configure(with: image)
+        return cell
+    }
+}
+
+// MARK:  Extension: UICollectionViewDelegate
+extension HomeViewController: UICollectionViewDelegate {
+    func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
+       
+        }
+    }
+
+// MARK:  Extension: UICollectionViewDelegateFlowLayout
+extension HomeViewController: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let flowLayout = collectionViewLayout as! UICollectionViewFlowLayout
+        
+        let totalSpace = flowLayout.sectionInset.left
+        + flowLayout.sectionInset.right
+        + flowLayout.minimumInteritemSpacing
+        
+        let width = Int((collectionView.bounds.width - totalSpace) / 2)
+        let height = 240
+        
+        return CGSize(width: width, height: height)
+    }
+}
+
+// MARK: - Extension: HomeViewModelDelegate
+extension HomeViewController: HomeViewModelDelegate {
+    func fetchedImages(_ images: [Image]) {
+        self.images = images
+        DispatchQueue.main.async {
+            self.collectionView.reloadData()
+        }
+    }
+    
+    func showError(_ receivedError: Error) {
+        DispatchQueue.main.async {
+            let alert = UIAlertController(title: "Error", message: receivedError.localizedDescription, preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default))
+            self.present(alert, animated: true)
+        }
+    }
+    
+    func navigateToImageDetails(with image: Image) {
+        //
+    }
+}
+
+#Preview {
+    HomeViewController()
 }
---
+++ Pixify/Scenes/Home/ImagesCollectionViewCell/ImagesCollectionViewCell.swift
@@ -0,0 +1,100 @@
+//
+//  ImagesCollectionViewCell.swift
+//  Pixify
+//
+//  Created by Ani's Mac on 27.02.24.
+//
+
+import UIKit
+
+final class ImagesCollectionViewCell: UICollectionViewCell {
+    // MARK: - Properties
+    private var image: Image?
+    
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        return imageView
+    }()
+  
+    private let authorNameLabel: UILabel = {
+        let label = UILabel()
+        label.textAlignment = .left
+        label.textColor = .customTextColor.withAlphaComponent(0.6)
+        label.numberOfLines = 1
+        label.font = UIFont.systemFont(ofSize: 14, weight: .medium)
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }()
+
+    // MARK: - Init
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        addSubview()
+        setupConstraints()
+        setupCellAppearance()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    // MARK: - CellLifeCycle
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        imageView.image = nil
+        authorNameLabel.text = nil
+    }
+    
+    // MARK: - Private Methods
+    private func addSubview() {
+        contentView.addSubview(imageView)
+        contentView.addSubview(authorNameLabel)
+    }
+    
+    private func setupConstraints() {
+        NSLayoutConstraint.activate([
+            imageView.topAnchor.constraint(equalTo: contentView.topAnchor),
+            imageView.leadingAnchor.constraint(equalTo: contentView.leadingAnchor),
+            imageView.trailingAnchor.constraint(equalTo: contentView.trailingAnchor),
+            imageView.heightAnchor.constraint(equalToConstant: 200),
+            
+            authorNameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor, constant: 12),
+            authorNameLabel.centerXAnchor.constraint(equalTo: contentView.centerXAnchor)
+        ])
+    }
+    
+    private func setupCellAppearance() {
+        layer.borderColor = UIColor.gray.withAlphaComponent(0.1).cgColor
+        layer.borderWidth = 1.0
+        layer.cornerRadius = 12
+        
+        contentView.layer.cornerRadius = layer.cornerRadius
+        contentView.layer.masksToBounds = true
+        
+        layer.shadowColor = UIColor.black.cgColor
+        layer.shadowOffset = CGSize(width: 0, height: 4)
+        layer.shadowRadius = 6.0
+        layer.shadowOpacity = 0.1
+        layer.masksToBounds = false
+    }
+    
+    private func setImage(from url: String) {
+        NetworkManager.shared.downloadImage(from: url) { [weak self] image in
+            DispatchQueue.main.async {
+                self?.imageView.image = image
+            }
+        }
+    }
+    
+    // MARK: - Configuration
+    func configure(with image: Image) {
+        self.image = image
+        authorNameLabel.text = image.user
+        
+        if let imageURL = image.largeImageURL {
+            setImage(from: imageURL)
+        }
+    }
+}
+
