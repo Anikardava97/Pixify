8add88d183f790e5d57818a7330ecb5268daf0c2
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-02-27 03:26:12 -0800

    Add search controller

--- Pixify/Managers/ApiManager.swift
+++ Pixify/Managers/ApiManager.swift
@@ -10,4 +10,10 @@ import Foundation
 struct ApiManager {
     static let baseUrl = "https://pixabay.com/api/?key="
     static let apiKey = "42585935-49ef4973398606c4a226ccd58"
+    
+    static func searchParameters(for query: String? = nil) -> String {
+        let baseParameters = "&image_type=photo&pretty=true"
+        guard let query, !query.isEmpty else { return baseParameters }
+        return "&q=\(query.addingPercentEncoding(withAllowedCharacters: .urlQueryAllowed) ?? "")\(baseParameters)"
+    }
 }
