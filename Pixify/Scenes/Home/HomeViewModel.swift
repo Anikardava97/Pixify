9b58801760a332bf95a5e5f1f3d4aa16eaffdd91
Anikardava97 <145837570+Anikardava97@users.noreply.github.com>
2024-02-28 08:23:24 -0800

    Implemented search functionality in the HomeViewModel to fetch images based on the search query

--- Pixify/Scenes/Home/HomeViewModel.swift
+++ Pixify/Scenes/Home/HomeViewModel.swift
@@ -13,6 +13,10 @@ final class HomeViewModel: ObservableObject {
     @Published private var error: String?
     @Published private var imagesURL = ApiManager.baseUrl + ApiManager.apiKey
     
+    @Published var currentPage = 1
+    @Published private var isFetchingImages = false
+    @Published private var hasMoreImages = true
+    
     @Published var selectedImage: Image?
     @Published var isDetailsViewPresented = false
     
@@ -26,6 +30,39 @@ final class HomeViewModel: ObservableObject {
             switch result {
             case .success(let fetchedImages):
                 self?.images = fetchedImages.hits
+            case .failure(let error):
+                self?.error = error.localizedDescription
+            }
+        }
+    }
+    
+    // MARK: - Methods
+    func fetchImages(searchQuery: String? = nil, isNewSearch: Bool = false) {
+        guard !isFetchingImages, hasMoreImages else { return }
+        
+        if isNewSearch {
+            images = []
+            currentPage = 1
+            hasMoreImages = true
+        }
+        
+        isFetchingImages = true
+        
+        var url = imagesURL + "&page=\(currentPage)&per_page=20"
+        if let query = searchQuery, !query.isEmpty {
+            url = ApiManager.baseUrl + ApiManager.apiKey + ApiManager.searchParameters(for: query) + "&page=\(currentPage)&per_page=20"
+        }
+        
+        NetworkManager.shared.fetch(from: url) { [weak self] (result: Result<ImageResponse, NetworkError>) in
+            self?.isFetchingImages = false
+            switch result {
+            case .success(let fetchedImages):
+                if self?.images == nil {
+                    self?.images = []
+                }
+                self?.images.append(contentsOf: fetchedImages.hits)
+                self?.hasMoreImages = !fetchedImages.hits.isEmpty
+                self?.currentPage += 1
             case .failure(let error):
                 self?.error = error.localizedDescription
             }
